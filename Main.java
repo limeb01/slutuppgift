ecc48dae72b3f98471003f22559cfad571c3e180
limeb01 <emiboi2001@gmail.com>
2020-05-07 15:00:59 +0200

    Uppdaterat lite

--- Main.java
+++ Main.java
@@ -9,76 +9,108 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.scene.shape.;
-import javafx.scene.Scene.;
+import javafx.scene.shape.*;
+import javafx.scene.Scene.*;
 
 
-	import java.awt.*;
-	import java.util.ArrayList;
+import java.awt.*;
+import java.util.ArrayList;
 
-	public class Main extends Application {
+public class Main extends Application {
 
-	    ArrayList <Rectangle> rectangleArrayList = new ArrayList<>();
+    Boolean isPlayer1Playing;
 
-	    ArrayList <Integer> test = new ArrayList<>();
+    ArrayList <Rectangle> rectangleArrayList = new ArrayList<>();
 
-	    public static void main(String[] args) {
-	        launch(args);
-	    }
+    ArrayList <Integer> test = new ArrayList<>();
 
-
-
-	    @Override
-	    public void start(Stage primaryStage) {
-
-	        VBox menu = new VBox();
-	        menu.setPrefWidth(500);
-	        menu.setPrefHeight(500);
-	        menu.setAlignment(Pos.CENTER);
-
-
-	        Scene menuScene = new Scene(menu,500,500);
-
-
-	        Button startGameButton = new Button("Start Game");
-	        startGameButton.setOnAction(event -> {
-	            primaryStage.setScene(gameScene());
-	            System.out.println("HEHE");
-	        });
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 
 
-	       startGameButton.setPrefSize(300,100);
-	       menu.getChildren().add(startGameButton);
+    @Override
+    public void start(Stage primaryStage) {
+
+        VBox menu = new VBox();
+        menu.setPrefWidth(500);
+        menu.setPrefHeight(500);
+        menu.setAlignment(Pos.CENTER);
 
 
-	        primaryStage.setScene(menuScene);
-	        primaryStage.show();
+        Scene menuScene = new Scene(menu,500,500);
 
 
-
-	    }
-	    public Scene gameScene(){
-
-	        HBox grid = new HBox();
-	        grid.setMinSize(500,500);
-	        Scene gameScene = new Scene(grid);
-
-	        Rectangle gridRectangle = new Rectangle(100,100);
-	        gridRectangle.setFill(Color.BLACK);
-	        gridRectangle.setStroke(Color.WHITE);
-	        Rectangle test = gridRectangle;
-
-
-	            grid.getChildren().addAll(gridRectangle,test);
+        Button startGameButton = new Button("Start Game");
+        startGameButton.setOnAction(event -> {
+            primaryStage.setScene(gameScene());
+        });
+        Button exitGameButton = new Button("Exit Game");
 
 
 
+       startGameButton.setPrefSize(300,100);
+       menu.getChildren().addAll(startGameButton,exitGameButton);
 
-	        return gameScene;
-	    }
+
+        primaryStage.setScene(menuScene);
+        primaryStage.show();
 
 
-	}
+
+    }
+    public Scene gameScene(){
+
+        GridPane grid = new GridPane();
+        grid.setMinSize(500,500);
+        Scene gameScene = new Scene(grid);
+        int x=0;
+        int temp;
+
+        for (int i = 0; i < 9 ; i++) {
+            if (i%3==0){
+                x++;
+            }
+            Rectangle gridRectangle = new Rectangle(200,200);
+            gridRectangle.setFill(Color.BLACK);
+            gridRectangle.setStroke(Color.WHITE);
+
+            gridRectangle.setOnMousePressed(event -> {
+
+                gridRectangle.setFill(Color.BLUE);
+
+
+            });
+            rectangleArrayList.add(gridRectangle);
+            grid.addRow(x,rectangleArrayList.get(i));
+
+            }
+
+
+
+        return gameScene;
+
+        }
+
+
+
+    public void drawCross(){
+
+    }
+    public void drawCircle(){
+
+    }
+    public void game(){
+        while (true){
+
+
+
+        }
+
+    }
+
+
+}
 
 	
--- planering.java
+++ planering.java
@@ -7,3 +7,11 @@ Jag ska programmera ett tre i rad. Jag kommer använda javafx för det grafiska.
 vecka 18: göra klart planering, mockup
 vecka 19: börja koda
 vecka 20: koda klart
+
+Uppdatering: Jag glömde lägga in min mockup. Jag gjorde den för någon dag sedan och använde paint för att spara tid.
+Mockup: https://gyazo.com/11ccacb8f0613ed749dadd78a981eff4 
+
+Uppdatering: Jag har haft massor av problem att hitta filerna på datorn, de ligger inte där de borde. Men eftersom jag redan har gjort en commit så tar jag
+gamla koden och lägger in den nya i Main.java. Jag har dock inte gjort så mycket, ändrat lite mått osv(testar mig fram lite).
+
+ 
