29f63b5964e0fdc8ba5147927cabb8d95951946c
limeb01 <emiboi2001@gmail.com>
2020-05-10 15:45:44 +0200

    fdsfsd

--- Main.java
+++ Main.java
@@ -4,13 +4,17 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.shape.*;
 import javafx.scene.Scene.*;
+import javafx.scene.text.Text;
+
 
 
 import java.awt.*;
@@ -18,11 +22,15 @@ import java.util.ArrayList;
 
 public class Main extends Application {
 
-    Boolean isPlayer1Playing;
+    Boolean isPlayer1Playing = true;
 
     ArrayList <Rectangle> rectangleArrayList = new ArrayList<>();
 
-    ArrayList <Integer> test = new ArrayList<>();
+    ArrayList <Circle> circleArray = new ArrayList<>();
+
+    ArrayList <Text> crossArray = new ArrayList<>();
+
+    GridPane grid = new GridPane();
 
     public static void main(String[] args) {
         launch(args);
@@ -59,34 +67,72 @@ public class Main extends Application {
 
 
 
+
     }
     public Scene gameScene(){
-
-        GridPane grid = new GridPane();
+        grid = new GridPane();
         grid.setMinSize(500,500);
         Scene gameScene = new Scene(grid);
         int x=0;
-        int temp;
+
 
         for (int i = 0; i < 9 ; i++) {
             if (i%3==0){
                 x++;
             }
             Rectangle gridRectangle = new Rectangle(200,200);
+
             gridRectangle.setFill(Color.BLACK);
             gridRectangle.setStroke(Color.WHITE);
 
+            final int arrayIndex = i;
+
             gridRectangle.setOnMousePressed(event -> {
 
-                gridRectangle.setFill(Color.BLUE);
+                System.out.println("еееееееееееееееееее");
+                if (isPlayer1Playing){
+                    circleArray.get(arrayIndex).setVisible(true);
+                    isPlayer1Playing = false;
+                }else {
+                    crossArray.get(arrayIndex).setVisible(true);
+                    isPlayer1Playing = true;
+                }
+
 
 
             });
+            Circle test = new Circle();
+            test.setRadius(80);
+            test.setStroke(Color.WHITE);
+            test.setStrokeWidth(20);
+            test.setFill(Color.BLACK);
+            test.setVisible(false);
+
+
+            Text cross = new Text("X");
+            cross.setStyle("-fx-font-size:150" );
+            cross.setLineSpacing(0);
+            cross.setVisible(false);
+            cross.setFill(Color.RED);
+
+            StackPane h = new StackPane();
+            h.setPrefSize(100,100);
+            h.getChildren().addAll(gridRectangle,test,cross);
+            crossArray.add(cross);
+            circleArray.add(test);
             rectangleArrayList.add(gridRectangle);
-            grid.addRow(x,rectangleArrayList.get(i));
+
+
+
+            grid.addRow(x,h);
+
+
+
+
+
+
 
             }
-
 
 
         return gameScene;
@@ -96,6 +142,9 @@ public class Main extends Application {
 
 
     public void drawCross(){
+        Circle test = new Circle();
+
+
 
     }
     public void drawCircle(){
@@ -112,5 +161,6 @@ public class Main extends Application {
 
 
 }
+
 
 	
