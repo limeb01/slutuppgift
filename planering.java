b5d2a6e30a10f9557269ac96e7f90d0316115c38
limeb01 <emiboi2001@gmail.com>
2020-05-18 13:04:32 +0200

    La in classdiagram i planeringen.

--- planering.java
+++ planering.java
@@ -8,6 +8,8 @@ vecka 18: göra klart planering, mockup
 vecka 19: börja koda
 vecka 20: koda klart
 
+Detta är mitt classdiagram: https://gyazo.com/683baa403388d9a8bc81b3d8e88749d5
+
 Uppdatering: Jag glömde lägga in min mockup. Jag gjorde den för någon dag sedan och använde paint för att spara tid.
 Mockup: https://gyazo.com/11ccacb8f0613ed749dadd78a981eff4 
 
